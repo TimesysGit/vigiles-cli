1e1621d924b44cce85b12c72538d07db3c4a1b04
Wesley <wesley.paglia@timesys.com>
2024-12-04 12:50:04 -0500

    Added support for create folder API

--- src/lib/timesys/vigiles/folders.py
+++ src/lib/timesys/vigiles/folders.py
@@ -33,3 +33,34 @@ def get_folders(group_token=None, folder_token=None):
 
     resource = "/api/v1/vigiles/folders"
     return timesys.llapi.GET(resource, data_dict=data)
+
+
+def create_folder(folder_name, description=None, group_token=None, folder_token=None):
+    """**Access to this route requires a Vigiles prime subscription..**
+
+    Create a new folder with given group/folder token
+
+    Returns
+    -------
+    dict
+        Dictionary of new folder information with keys:
+            "folder_name", "group_token", "folder_token", "description", "creation_date"
+    """
+
+    data = {
+        "folder_name": folder_name,
+    }
+    group_token = group_token or timesys.llapi.group_token
+    folder_token = folder_token or timesys.llapi.folder_token
+
+    if description:
+        data["description"] = description
+    if group_token and not folder_token:
+        data["group_token"] = group_token
+    if folder_token:
+        data["folder_token"] = folder_token
+
+    return timesys.llapi.POST(
+        "/api/v1/vigiles/folders",
+        data_dict=data
+    )
