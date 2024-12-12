1b0037503a6d860b4172b30433dee122c181b62e
suvam.bhunia <suvam.bhunia@timesys.com>
2024-12-12 21:15:23 +0000

    api: Fix issue where folder token was not applied correctly to retrieve the folder list

--- src/lib/timesys/vigiles/folders.py
+++ src/lib/timesys/vigiles/folders.py
@@ -4,13 +4,16 @@
 import timesys
 
 
-def get_folders():
+def get_folders(product_token=None, folder_token=None):
     """**Access to this route requires a Vigiles prime subscription..**
 
     Get all folders that are owned by the current user.
 
     If a product token is configured on the llapi object, only folders belonging
     to that product will be returned.
+
+    If a folder token is configured on the llapi object, only folders belonging
+    to that folder will be returned.
 
     Returns
     -------
@@ -20,9 +23,13 @@ def get_folders():
     """
 
     data = {}
-    product_token = timesys.llapi.product_token
-    if product_token:
+    product_token = product_token or timesys.llapi.product_token
+    folder_token = folder_token or timesys.llapi.folder_token
+
+    if product_token and not folder_token:
         data["product_token"] = product_token
+    if folder_token:
+        data["folder_token"] = folder_token
 
     resource = "/api/v1/vigiles/folders"
     return timesys.llapi.GET(resource, data_dict=data)
